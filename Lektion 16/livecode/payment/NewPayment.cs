f68b5f4cac7e1f23662045db6051bf785bca6325
stefan <skqu@iba.dk>
2026-03-09 13:51:30 +0100

    Added lecture note

---
+++ Lektion 16/livecode/payment/NewPayment.cs
@@ -0,0 +1,16 @@
+namespace Livecode.Payment
+{
+    class NewPayment
+    {
+        public NewPayment()
+        {
+            
+        }
+        
+
+        public string smth()
+        {
+            return "Hello from adaptee";
+        }
+    }
+}
---
+++ Lektion 16/solution/Program.cs
@@ -0,0 +1,20 @@
+﻿using Livecode.Payment;
+namespace Livecode
+{
+    class Program
+    {
+        public static void Main()
+        {
+            IPayment payment = new PaymentAdapter();
+
+            bool cardStatus =  payment.VerifyCard();
+            bool paymentStatus = payment.CreateTransaction(2, 10, "DKK");
+            bool verification = payment.VerifyPayment();
+
+            Console.WriteLine("cardStatus = " + cardStatus.ToString());
+            Console.WriteLine("paymentStatus = " + paymentStatus.ToString());
+            Console.WriteLine("Payment Verification = " + verification.ToString());
+        }
+    }
+
+}
