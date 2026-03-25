c5bbb4ce1b2e01b7dbdcc0ef2596e170f774c50b
stefan <skqu@iba.dk>
2026-03-25 09:55:44 +0100

    Cleanup

--- lektion-12/assets/slidev/default-Cs9KwbdI.js
+++
@@ -1,1 +0,0 @@
-import{o as t,e as o,i as s}from"../modules/vue-Wd0KeOlu.js";import{u as a}from"./context-CHDJCQPS.js";const r={class:"slidev-layout default"},u={__name:"default",setup(l){return a(),(e,n)=>(t(),o("div",r,[s(e.$slots,"default")]))}};export{u as _};
